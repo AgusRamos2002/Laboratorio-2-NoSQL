9a485d3be08f6edc7093049886977f4d420c0301
Damian Dalto <ddalto@geocom.com.uy>
2022-10-27 21:17:13 -0300

    se arregla funcion agregarRol

--- src/main/java/com/example/prueba/Usuario.java
+++ src/main/java/com/example/prueba/Usuario.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
@@ -17,5 +18,5 @@ public class Usuario {
     String password;
     String nombre;
     String apellido;
-    Set<String> roles;
+    Set<String> roles = new HashSet<>();
 }
