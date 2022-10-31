1bb61430dd7bc9e982b12210153d3157f39a7c76
Sebastian Miranda <sebastian.miranda@globant.com>
2022-10-31 19:41:29 -0300

    se agregan mensajes de respuesta segun letra

--- src/main/java/com/example/prueba/UsuarioController.java
+++ src/main/java/com/example/prueba/UsuarioController.java
@@ -1,14 +1,17 @@
 package com.example.prueba;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -28,13 +31,13 @@ public class UsuarioController {
     }
 
     @PostMapping
-    public void crear(@RequestBody Usuario usuario){
+    public ResponseEntity<String> crear(@RequestBody Usuario usuario){
         Optional<Usuario> optionalUsuario = usuarioRepository.findById(usuario.getEmail());
-        if(!optionalUsuario.isEmpty()){
-            ResponseEntity.badRequest();
-            throw new RuntimeException("Error Codigo 101: Usuario ya existente");
+        if(optionalUsuario.isPresent()){
+            return ResponseEntity.badRequest().body("Error Codigo 101: Usuario ya existente");
         }
         usuarioRepository.save(usuario);
+        return ResponseEntity.ok("Usuario registrado con exito");
     }
 
     @GetMapping("/{email}/{password}")
@@ -45,47 +48,56 @@ public class UsuarioController {
         ).build();
     }
 
-    @PutMapping
-    public void agregarRol(@RequestParam String email, @RequestParam String password, @RequestParam String rol){
-        Usuario usuario = getUsuario(email, password);
-
-        usuario.getRoles().add(rol);
-        usuarioRepository.save(usuario);
+    @PutMapping("/agregarRoles")
+    public ResponseEntity<String> agregarRol(@RequestBody UsuarioRolDTO agregarRolDTO){
+        try {
+            Usuario usuario = getUsuario(agregarRolDTO.getEmail(), agregarRolDTO.getPassword());
+            usuario.getRoles().addAll(agregarRolDTO.getRoles());
+            usuarioRepository.save(usuario);
+        }
+        catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok("Roles agregados con exito");
     }
 
-    @PutMapping("/{email}")
-    public ResponseEntity<String> eliminarRolesUsuario(@RequestBody UsuarioEliminarRolDTO eliminarRolDTO){
-        Usuario usuario = getUsuario(eliminarRolDTO.getEmail(), eliminarRolDTO.getPassword());
+    @PutMapping("/eliminarRoles")
+    public ResponseEntity<String> eliminarRolesUsuario(@RequestBody UsuarioRolDTO eliminarRolDTO){
+        Usuario usuario;
+        try {
+            usuario = getUsuario(eliminarRolDTO.getEmail(), eliminarRolDTO.getPassword());
 
-        for (String rol: eliminarRolDTO.getRoles())
-            if(!usuario.getRoles().contains(rol)) {
-            return ResponseEntity.badRequest().body("103: El usuario no contiene el ROL: " + rol);
         }
+        catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        //eliminarRolDTO.getRoles().forEach(rol -> {
+        //    if (!usuario.getRoles().contains(rol))
+        //        ResponseEntity.badRequest().body("103: El usuario no contiene el ROL: " + rol);
+        //});
+
+       for (String rol : eliminarRolDTO.getRoles())
+            if (!usuario.getRoles().contains(rol))
+                return ResponseEntity.badRequest().body("103: El usuario no contiene el ROL: " + rol);
 
         usuario.getRoles().removeAll(eliminarRolDTO.getRoles());
-
-
         usuarioRepository.save(usuario);
 
         return ResponseEntity.ok("Actualizado");
     }
 
-
-    private Usuario getUsuario(String email, String password) {
+    private Usuario getUsuario(String email, String password) throws RuntimeException{
         Optional<Usuario> optionalUsuario = usuarioRepository.findById(email);
         if(optionalUsuario.isEmpty()) {
-            ResponseEntity.badRequest();
             throw new RuntimeException("Error Codigo 102: Usuario no encontrado");
         }
 
         Usuario usuario = optionalUsuario.get();
 
         if(!optionalUsuario.get().getPassword().equals(password)) {
-            ResponseEntity.badRequest();
             throw new RuntimeException("Error Codigo 104: Constraseña incorrecta");
         }
         return usuario;
     }
-
 
 }
--- src/main/java/com/example/prueba/UsuarioRolDTO.java
+++ src/main/java/com/example/prueba/UsuarioRolDTO.java
@@ -1,12 +1,11 @@
 package com.example.prueba;
 
 import lombok.Data;
-import org.apache.coyote.http11.filters.SavedRequestInputFilter;
 
 import java.util.Set;
 
 @Data
-public class UsuarioEliminarRolDTO {
+public class UsuarioRolDTO {
     String email;
     String password;
     Set<String> roles;
