9a37a8ce2e6259351347bd79aa533f33afacbc9c
Damian Dalto <ddalto@geocom.com.uy>
2022-11-09 19:41:40 -0300

    otro fix

--- src/main/java/com/example/prueba/Usuario.java
+++ src/main/java/com/example/prueba/Usuario.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -11,6 +12,7 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document("usuarios")
 public class Usuario {
     @Id
     String email;
